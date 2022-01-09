a930867af1854defaf2fe4c5fd7cfad5014a7735
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2022-01-09 15:38:51 -0500

    create macro to run operations outside of models

---
+++ macros/resume_warehouse.sql
@@ -0,0 +1,11 @@
+{% macro resume(warehouse_name) %}
+
+    {% set sql %}
+        alter warehouse {{warehouse_name}} resume
+    {% endset %}
+
+    {% set table = run_query(sql) %}
+
+    {% do table.print_table() %}
+
+{% endmacro %}

e515c37dc65883d268a54397bd08c0bbd836cd40
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2022-01-09 14:58:47 -0500

    update model snowflake_customer_purchases to use source

--- models/example/snowflake_customer_purchases.sql
+++ models/example/snowflake_customer_purchases.sql
@@ -4,8 +4,8 @@ SELECT
     c.c_name,
     c.c_nationkey as nation,
     sum(o.o_totalprice) as total_order_price
-FROM "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER" c
-LEFT JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
+FROM {{ source('sample', 'customer') }} c
+LEFT JOIN {{ source('sample', 'orders') }} o
 ON c.c_custkey = o.o_custkey
 
 group by
--- target/compiled/learn_dbt/models/example/snowflake_customer_purchases.sql
+++ target/compiled/learn_dbt/models/example/snowflake_customer_purchases.sql
@@ -3,8 +3,8 @@ SELECT
     c.c_name,
     c.c_nationkey as nation,
     sum(o.o_totalprice) as total_order_price
-FROM "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER" c
-LEFT JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
+FROM snowflake_sample_data.tpch_sf1.customer c
+LEFT JOIN snowflake_sample_data.tpch_sf1.orders o
 ON c.c_custkey = o.o_custkey
 
 group by

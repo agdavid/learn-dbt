eb6e348c68217d79c284d462ff01e5579a3989d1
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2022-01-19 20:50:28 -0500

    create model for 100 customers from sample data

---
+++ models/example/snowflake_100_customer_list.sql
@@ -0,0 +1,12 @@
+select
+    top 100
+    c_customer_sk,
+    c_salutation,
+    c_first_name,
+    c_last_name,
+    c_email_address,
+    c_preferred_cust_flag,
+    c_birth_day,
+    c_birth_month,
+    c_birth_year
+from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."CUSTOMER"
